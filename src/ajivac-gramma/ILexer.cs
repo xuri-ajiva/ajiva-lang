36904f9de9699026674b878b41f6e6ffe796a65c
xuri02 <t0bl4s.durst@gmail.com>
2022-08-11 20:45:21 +0200

    Updated Lexer to have default precedence

--- src/ajivac-gramma/ILexer.cs
+++ src/ajivac-gramma/ILexer.cs
@@ -150,7 +150,61 @@ public class Lexer : ILexer
         return (char)_reader.Peek();
     }
 
-    public Lexer(string text, Dictionary<TokenType, int> binOpPrecedence)
+    public Lexer(string text) : this(text, 
+        //https://en.cppreference.com/w/c/language/operator_precedence
+        new Dictionary<TokenType, int>() {
+            [TokenType.Comma] = 10,
+
+            [TokenType.Assign] = 20,
+
+            [TokenType.AddAssign] = 30,
+            [TokenType.SubAssign] = 30,
+
+            [TokenType.MulAssign] = 30,
+            [TokenType.DivAssign] = 30,
+            [TokenType.ModAssign] = 30,
+
+            [TokenType.AndAssign] = 30,
+            [TokenType.OrAssign] = 30,
+            [TokenType.XorAssign] = 30,
+
+            [TokenType.Question] = 40,
+            [TokenType.Colon] = 40,
+
+            [TokenType.Or] = 50,
+            [TokenType.Xor] = 60,
+            [TokenType.And] = 70,
+            [TokenType.BitOr] = 80,
+            [TokenType.BitXor] = 90,
+            [TokenType.BitAnd] = 100,
+
+            [TokenType.DoubleEquals] = 110,
+            [TokenType.NotEqual] = 110,
+
+            [TokenType.Greater] = 120,
+            [TokenType.GreaterEqual] = 120,
+            [TokenType.Less] = 120,
+            [TokenType.LessEqual] = 120,
+
+            [TokenType.ShiftLeft] = 130,
+            [TokenType.ShiftRight] = 130,
+
+            [TokenType.Plus] = 140,
+            [TokenType.Minus] = 140,
+
+            [TokenType.Star] = 150,
+            [TokenType.Slash] = 150,
+            [TokenType.Percent] = 150,
+
+            [TokenType.Not] = 160,
+            [TokenType.Negate] = 160,
+
+            [TokenType.Increment] = 170,
+            [TokenType.Decrement] = 170,
+        })
+    {
+    }
+    private Lexer(string text, Dictionary<TokenType, int> binOpPrecedence)
     {
         _reader = new StringReader(text);
         _text = text;
--- src/ajivac-llvm/Program.cs
+++ src/ajivac-llvm/Program.cs
@@ -28,60 +28,9 @@ else
 a = a + fac(10)
 System.Console.WriteLine(a)
 ";
-ILexer lexer = new Lexer(src,
-    //https://en.cppreference.com/w/c/language/operator_precedence
-    new Dictionary<TokenType, int>() {
-        [TokenType.Comma] = 10,
+ILexer lexer = new Lexer(src);
 
-        [TokenType.Assign] = 20,
-
-        [TokenType.AddAssign] = 30,
-        [TokenType.SubAssign] = 30,
-
-        [TokenType.MulAssign] = 30,
-        [TokenType.DivAssign] = 30,
-        [TokenType.ModAssign] = 30,
-
-        [TokenType.AndAssign] = 30,
-        [TokenType.OrAssign] = 30,
-        [TokenType.XorAssign] = 30,
-
-        [TokenType.Question] = 40,
-        [TokenType.Colon] = 40,
-
-        [TokenType.Or] = 50,
-        [TokenType.Xor] = 60,
-        [TokenType.And] = 70,
-        [TokenType.BitOr] = 80,
-        [TokenType.BitXor] = 90,
-        [TokenType.BitAnd] = 100,
-
-        [TokenType.DoubleEquals] = 110,
-        [TokenType.NotEqual] = 110,
-
-        [TokenType.Greater] = 120,
-        [TokenType.GreaterEqual] = 120,
-        [TokenType.Less] = 120,
-        [TokenType.LessEqual] = 120,
-
-        [TokenType.ShiftLeft] = 130,
-        [TokenType.ShiftRight] = 130,
-
-        [TokenType.Plus] = 140,
-        [TokenType.Minus] = 140,
-
-        [TokenType.Star] = 150,
-        [TokenType.Slash] = 150,
-        [TokenType.Percent] = 150,
-
-        [TokenType.Not] = 160,
-        [TokenType.Negate] = 160,
-
-        [TokenType.Increment] = 170,
-        [TokenType.Decrement] = 170,
-    });
-
-Console.WriteLine(src);
+Console.WriteLine(src.Replace("\r\n", "\r\n>  "));
 var parser = new Parser(lexer);
 var ast = parser.ParseAll();
 Console.WriteLine(ast.GetType() + ": " + ast.Span);
