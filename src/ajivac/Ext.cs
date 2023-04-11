279313840de63da8ea1bf52dadef2f66e2b05b07
Tobias Durst <t0bl4s.durst@gmail.com>
2023-04-11 16:36:29 +0200

    Compiler Run / Compile Commands

---
+++ src/ajivac/Ext.cs
@@ -0,0 +1,114 @@
+﻿using ajivac_il;
+using ajivac_lib;
+using ajivac_lib.AST;
+using ajivac_lib.Semantics;
+using Spectre.Console;
+
+public static class Ext
+{
+    public static SourceFile SourceFile(string path, out string name)
+    {
+        var source = new SourceFile(File.ReadAllText(path), path);
+        name = Path.GetFileNameWithoutExtension(path);
+        return source;
+    }
+
+    public static void Print(this SourceFile source)
+    {
+        AnsiConsole.Write(new Panel(new Text(source.Text).Fold())
+            .Header("Source"));
+    }
+
+    public static void RunWithOutput(this ILCodeGenerator ilGenerator, string name)
+    {
+        AnsiConsole.MarkupLine($"[green]Running[/] {name}");
+        try
+        {
+            ilGenerator.Run();
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.WriteException(e);
+        }
+        finally
+        {
+            AnsiConsole.MarkupLine($"[green]Finished[/] {name}");
+        }
+    }
+
+    public static void CompileWithOutput(this Compiler compiler, string name)
+    {
+        AnsiConsole.Status().AutoRefresh(true)
+            .Spinner(Spinner.Known.Dots).Start("Compiling..."
+                , ctx =>
+                {
+                    AnsiConsole.MarkupLine($"[green]Compiling {name}[/]");
+                    ctx.Status("Parsing...");
+                    compiler.ParseAll();
+                    AnsiConsole.MarkupLine("[green]Parsing Done[/]");
+                    ctx.Status("Semantic Analysis...");
+                    compiler.Analyze();
+                    AnsiConsole.MarkupLine("[green]Semantic Analysis Done[/]");
+                });
+    }
+
+    public static void GenerateWithOutput(this ILCodeGenerator generator, Compiler compiler, string name)
+    {
+        AnsiConsole.Status().AutoRefresh(true)
+            .Spinner(Spinner.Known.Dots).Start("Generating..."
+                , ctx =>
+                {
+                    AnsiConsole.MarkupLine($"[green]Generating {name}[/]");
+                    ctx.Status("Generating...");
+                    compiler.Ast.Accept(generator, ref IlFrame.Empty);
+                    AnsiConsole.MarkupLine("[green]Generating Done[/]");
+                    ctx.Status("Finishing...");
+                    generator.Finish();
+                    AnsiConsole.MarkupLine("[green]Finishing Done[/]");
+                });
+    }
+
+    public static void SaveWithOutput(this ILCodeGenerator ilGenerator, string path, string name)
+    {
+        AnsiConsole.Status().AutoRefresh(true)
+            .Spinner(Spinner.Known.Dots).Start("Saving..."
+                , ctx =>
+                {
+                    var save = Path.GetFullPath(path);
+                    AnsiConsole.MarkupLine($"[green]Saving {name}[/]");
+                    ilGenerator.Save(save);
+                    AnsiConsole.MarkupLine($"[green]Saved[/] {save}");
+                    //write .runtimeconfig.json
+                    var runtimeConfigPath = Path.ChangeExtension(save, ".runtimeconfig.json");
+                    ctx.Status("[green]Writing runtimeconfig.json[/]");
+                    var runtimeConfig = """
+                                        {
+                                          "runtimeOptions": {
+                                            "tfm": "net7.0",
+                                            "framework": {
+                                              "name": "Microsoft.NETCore.App",
+                                              "version": "7.0.0"
+                                            }
+                                          }
+                                        }
+                                        """;
+                    File.WriteAllText(runtimeConfigPath, runtimeConfig);
+                    AnsiConsole.MarkupLine($"[green]Written[/] {runtimeConfigPath}");
+                });
+    }
+
+    public static void PrintTree(this IAstNode compilerAst)
+    {
+        AnsiConsole.MarkupLine("[green]AST[/]");
+        var root = compilerAst.ToTree().First();
+        AnsiConsole.Write(root);
+    }
+
+    public static IEnumerable<Tree> ToTree(this IAstNode node)
+    {
+        var tree = new Tree(node.GetType().ToString());
+        foreach (var child in node.Children)
+            tree.AddNodes(child.ToTree());
+        yield return tree;
+    }
+}
--- src/ajivac/Program.cs
+++ src/ajivac/Program.cs
@@ -3,18 +3,23 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using ajivac_il;
 using ajivac_lib;
-using ajivac_lib.AST;
-using ajivac_lib.Semantics;
 using ajivac_llvm;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using Spectre.Console.Rendering;
 
 AnsiConsole.Write(new FigletText("Ajiva Compiler").Color(Color.Blue));
-var app = new CommandApp<AjivaCompiler>();
+var app = new CommandApp();
+app.Configure(c =>
+{
+    c.SetApplicationName("Ajiva Compiler");
+    c.SetApplicationVersion("1.0.0");
+    c.AddCommand<RunCommand>("run");
+    c.AddCommand<CompileCommand>("compile");
+});
 return app.Run(args);
 
-internal sealed class AjivaCompiler : Command<AjivaCompiler.Settings>
+internal sealed class RunCommand : Command<RunCommand.Settings>
 {
     public sealed class Settings : CommandSettings
     {
@@ -35,6 +40,54 @@ internal sealed class AjivaCompiler : Command<AjivaCompiler.Settings>
         [CommandOption("-t|--print-tree")]
         [DefaultValue(false)]
         public bool PrintTree { get; set; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var source = Ext.SourceFile(settings.InPath, out var name);
+        var compiler = new Compiler(source, new SpectreDiagnostics());
+        if (settings.PrintSource)
+            source.Print();
+
+        compiler.CompileWithOutput(name);
+
+        if (settings.PrintTree)
+            compiler.Ast.PrintTree();
+
+        var interpreter = new Interpreter(s => Debug.WriteLine(s));
+        interpreter.Load(compiler.RuntimeState);
+        var ilGenerator = new ILCodeGenerator(name, interpreter);
+        ilGenerator.GenerateWithOutput(compiler, name);
+
+        ilGenerator.RunWithOutput(name);
+        if (settings.OutPath is not null)
+            ilGenerator.SaveWithOutput(settings.OutPath, name);
+
+        AnsiConsole.MarkupLine($"[green]Compiled[/] {name}");
+        return 0;
+    }
+}
+internal sealed class CompileCommand : Command<CompileCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to the file to compile")]
+        [CommandArgument(0, "[file.aj]")]
+        public string InPath { get; init; }
+
+        [Description("Path where to save the compiled file")]
+        [CommandArgument(1, "[file.dll]")]
+        public string? OutPath { get; init; }
+
+        [Description("Print the Source")]
+        [CommandOption("-s|--print-source")]
+        [DefaultValue(false)]
+        public bool PrintSource { get; set; }
+
+        [Description("Print the AST")]
+        [CommandOption("-t|--print-tree")]
+        [DefaultValue(false)]
+        public bool PrintTree { get; set; }
 
         [Description("Run the Program")]
         [CommandOption("-r|--run")]
@@ -44,96 +97,27 @@ internal sealed class AjivaCompiler : Command<AjivaCompiler.Settings>
 
     public override int Execute(CommandContext context, Settings settings)
     {
-        var source = new SourceFile(File.ReadAllText(settings.InPath), settings.InPath);
-        var name = Path.GetFileNameWithoutExtension(settings.InPath);
+        var source = Ext.SourceFile(settings.InPath, out var name);
         var compiler = new Compiler(source, new SpectreDiagnostics());
         if (settings.PrintSource)
-            AnsiConsole.Write(new Panel(new Text(source.Text).Fold())
-                .Header("Source"));
-        AnsiConsole.Status().AutoRefresh(true)
-            .Spinner(Spinner.Known.Dots).Start("Compiling..."
-                , ctx =>
-                {
-                    AnsiConsole.MarkupLine($"[green]Compiling {name}[/]");
-                    ctx.Status("Parsing...");
-                    compiler.ParseAll();
-                    AnsiConsole.MarkupLine($"[green]Parsing Done[/]");
-                    ctx.Status("Semantic Analysis...");
-                    compiler.Analyze();
-                    AnsiConsole.MarkupLine($"[green]Semantic Analysis Done[/]");
-                });
+            source.Print();
+
+        compiler.CompileWithOutput(name);
 
         if (settings.PrintTree)
-        {
-            AnsiConsole.MarkupLine("[green]AST[/]");
-            PrintTree(compiler.Ast);
-        }
+            compiler.Ast.PrintTree();
+
         var interpreter = new Interpreter(s => Debug.WriteLine(s));
         interpreter.Load(compiler.RuntimeState);
         var ilGenerator = new ILCodeGenerator(name, interpreter);
-        ilGenerator.Visit((RootNode)compiler.Ast);
-        ilGenerator.Finish();
+        ilGenerator.GenerateWithOutput(compiler, name);
+
         if (settings.Run)
-        {
-            AnsiConsole.MarkupLine($"[green]Running[/] {name}");
-            try
-            {
-                ilGenerator.Run();
-            }
-            catch (Exception e)
-            {
-                AnsiConsole.WriteException(e);
-            }
-            finally
-            {
-                AnsiConsole.MarkupLine($"[green]Finished[/] {name}");
-            }
-        }
+            ilGenerator.RunWithOutput(name);
         if (settings.OutPath is not null)
-        {
-            var save = Path.GetFullPath(settings.OutPath);
-            AnsiConsole.MarkupLine($"[green]Saving[/] {name} to {save}");
-            ilGenerator.Save(save);
-        }
+            ilGenerator.SaveWithOutput(settings.OutPath, name);
+
         AnsiConsole.MarkupLine($"[green]Compiled[/] {name}");
         return 0;
-    }
-
-    private void PrintTree(IAstNode compilerAst)
-    {
-        var root = compilerAst.ToTree().First();
-        AnsiConsole.Write(root);
-    }
-}
-internal class SpectreDiagnostics : Diagnostics
-{
-    public SpectreDiagnostics() : base(null, Sensitivity.Info)
-    {
-    }
-
-    public override void ReportError(SourceSpan location, string message, Sensitivity sensitivity)
-    {
-        const int max = 10;
-        AnsiConsole.MarkupLine($"[red]{sensitivity}[/]: {location.FullLocation()}");
-        AnsiConsole.WriteLine(Extent(location, max));
-        AnsiConsole.WriteLine($"{"",(max - 2)}~~^~~ {message}");
-    }
-
-    private string Extent(SourceSpan location, int max)
-    {
-        return (location with {
-            Position = (uint)Math.Max(0, location.Position - max),
-            Length = (uint)Math.Min(location.Length + max * 2, location.Source.Length - location.Position)
-        }).GetValue();
-    }
-}
-public static class Ext
-{
-    public static IEnumerable<Tree> ToTree(this IAstNode node)
-    {
-        var tree = new Tree(node.GetType().ToString());
-        foreach (var child in node.Children)
-            tree.AddNodes(child.ToTree());
-        yield return tree;
     }
 }
